a2a948565aa64becaabf725d5101fa3960110597
admim <admim@admims-MBP.fritz.box>
2020-08-10 16:00:17 +0200

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return <p>hello world.</p>;
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input
+          type="text"
+          onChange={() => {
+            console.log("I am clicked!");
+          }}
+        />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
